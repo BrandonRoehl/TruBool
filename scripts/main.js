fb99e74156d44486621623faee6db53030319d75
Brandon Roehl <BrandonRoehl@gmail.com>
2016-10-02 10:25:09 -0500

    The js class

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+class Board {
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+}
+
+window.addEventListener("load", function(){
+    alert("ready");
+});
+
+function repaint(){
+}
