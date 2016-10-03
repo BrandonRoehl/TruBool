9db7cdffae6be63ad66a27f1038e97da78a46e0a
Brandon Roehl <BrandonRoehl@gmail.com>
2016-10-03 11:55:24 -0500

    Progress

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,41 @@ class Board {
         this.width = width;
         this.height = height;
     }
+
+    boardWidth(){
+        return this.width - 20;
+    }
+
+    boardHeight(){
+        return this.width - 20;
+    }
+
+    boardX(){
+        return 10;
+    }
+
+    boardY(){
+        return 10;
+    }
 }
 
-window.addEventListener("load", function(){
-    alert("ready");
+var board;
+var canvas;
+
+window.addEventListener("DOMContentLoaded", function(){
+    board = new Board(window.innerWidth, window.innerHeight);
+    canvas = document.getElementById("canvas").getContext("2d");
+    repaint();
+    alert(board.boardWidth() + "x" + board.boardHeight());
 });
 
-function repaint(){
+function repaint(canvas){
+    clear(canvas);
+    canvas.fillRect(25,25,100,100);
+}
+
+function clear(canvas){
+    if (canvas != null){
+        canvas.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    }
 }
