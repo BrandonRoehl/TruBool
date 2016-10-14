158091a81d5562fdd817f83792356fe30f18344f
Brandon Roehl <BrandonRoehl@gmail.com>
2016-10-14 18:52:20 -0500

    Comment code the kind that all code should be now I can continue

--- scripts/main.js
+++ scripts/main.js
@@ -70,16 +70,21 @@ class Board {
      * @return game piece
      */
     getPiece(x, y) {
+        // Start by seeing if the piece is in the y height of the dock
         var tmp = (y - this.boardY - this.boardHeight - 10);
         if (tmp > 0 && tmp < this.unit) {
+            // Then check to see if there is a piece there for it
             tmp = this.getBoardX(x);
             if (0 <= tmp && tmp < this.pieces.length){
+                // If there is remove the piece from the list and return it
                 return this.pieces.splice(tmp, 1);
             }
         } else {
+            // Start by seing if the x, y is on the board
             var cx = this.getBoardX(x);
             var cy = this.getBoardY(y);
             if (this.onBoard(cx, cy)) {
+                // If it is remove it and return it
                 tmp = this.layout[cx][cy];
                 this.layout[cx][cy] = undefined;
                 return tmp;
@@ -88,37 +93,56 @@ class Board {
         return null;
     }
 
+    /**
+     * This sets a piece on the board based on screen cords to a piece id that is passed in
+     *
+     * @param x corodanate on the canvas that is drawn to
+     * @param y corodanate on the canvas that is drawn to
+     * @param p an id identifying what piece to place
+     */
     setPiece(x, y, p) {
+        // Make sure there is a piece and its not wire
         if (![0, null, undefined].includes(p)) {
             var cx = this.getBoardX(x);
             var cy = this.getBoardY(y);
+            // Check to see if the location is on the board and the board doesn't already have a piece there
             if (this.onBoard(cx, cy) && [0, null, undefined].includes(this.layout[cx][cy])) {
+                // If its good add it to the locaiton
                 this.layout[cx][cy] = p;
             } else {
+                // The spot was invalid so we should add it to the end of the palate so we don't loose it
                 this.pieces.push(p);
             }
         }
     }
 
+    /**
+     * This is for the drawing of wires on the board
+     *
+     * @param x corodanate on the canvas that is drawn to
+     * @param y corodanate on the canvas that is drawn to
+     * @param p an id identifying what piece was last clicked on
+     */
     toogleWire(x, y, piece) {
+        // If the piece that was given is a wire or nothing
         if ([0, null, undefined].includes(piece)) {
             var cx = this.getBoardX(x);
             var cy = this.getBoardY(y);
+            // If the spot was on the board and not a piece
             if (this.onBoard(cx, cy) && [0, null, undefined].includes(this.layout[cx][cy])) {
+                // Then set it to the oposite of the piece given
                 this.layout[cx][cy] = (piece == 0) ? undefined : 0;
             }
         }
     }
 
     draw(canvas){
-        canvas.fillStyle = "#000000";
-        canvas.fillRect(this.boardX ,this.boardY, this.boardWidth, this.boardHeight);
         var color = false;
         for(var x = 0; x < this.gameWidth; x++){
             if (this.gameWidth % 2 != 0) color = !color;
             for(var y = 0; y < this.gameHeight; y++){
                 color = !color;
-                canvas.fillStyle = color ? "#555555" : "#333333";
+                canvas.fillStyle = color ? "#111111" : "#222222";
                 canvas.fillRect(this.unit * x + this.boardX, this.unit * y + this.boardY, this.unit, this.unit);
                 var piece = pieceAsset(this.layout[x][y], false);
                 if (piece != null) {
@@ -128,7 +152,7 @@ class Board {
         }
         for(var x = 0; x < this.gameWidth; x++){
             color = !color;
-            canvas.fillStyle = color ? "#555555" : "#333333";
+            canvas.fillStyle = color ? "#111111" : "#222222";
             canvas.fillRect(this.unit * x + this.boardX, this.boardHeight + this.boardY + 10, this.unit, this.unit);
             if(x < this.pieces.length){
                 canvas.drawImage(pieceAsset(this.pieces[x], false), this.unit * x + this.boardX, this.boardHeight + this.boardY + 10, this.unit, this.unit);
@@ -224,6 +248,7 @@ document.addEventListener("DOMContentLoaded", function(event){
     setInterval(repaint, 100);
 });
 
+// TODO replace this with JSON at some point
 function toLogicArray(element, className){
     var inputsHTML = Array.from(element.getElementsByClassName(className));
     var inputs = new Array(inputsHTML.lenght);
