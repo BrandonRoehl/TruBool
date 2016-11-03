65f6183b01c565e79c7f710b8d74fa5324052df4
Brandon Roehl <BrandonRoehl@gmail.com>
2016-11-03 09:26:52 -0500

    Recomment in the correct stuff

--- scripts/draw.js
+++ scripts/draw.js
@@ -2,7 +2,7 @@ var FPS = 60;
 var canvas;
 
 function initCanvas() {
-    canvas = document.getElementsByTagName("canvas")[0].getContext("webgl");
+    canvas = document.getElementsByTagName("canvas")[0].getContext("2d");
 }
 
 function draw(board, canvas) {
@@ -13,30 +13,30 @@ function draw(board, canvas) {
         for (var y = 0; y < board.gameHeight; y++) {
             color = !color;
             canvas.fillStyle = color ? "#111111" : "#222222";
-            // canvas.fillRect(
-                // board.unit * x + board.boardX,
-                // board.unit * y + board.boardY,
-                // board.unit,
-                // board.unit
-            // );
+            canvas.fillRect(
+                board.unit * x + board.boardX,
+                board.unit * y + board.boardY,
+                board.unit,
+                board.unit
+            );
             if (board.layout[x][y] == 0) {
-                // canvas.drawImage(
-                    // wireAsset(board.wireStyle[x][y], true),
-                    // board.unit * x + board.boardX,
-                    // board.unit * y + board.boardY,
-                    // board.unit,
-                    // board.unit
-                // );
+                canvas.drawImage(
+                    wireAsset(board.wireStyle[x][y], true),
+                    board.unit * x + board.boardX,
+                    board.unit * y + board.boardY,
+                    board.unit,
+                    board.unit
+                );
             } else {
                 var piece = pieceAsset(board.layout[x][y], true);
                 if (piece != null) {
-                    // canvas.drawImage(
-                        // piece,
-                        // board.unit * x + board.boardX,
-                        // board.unit * y + board.boardY,
-                        // board.unit,
-                        // board.unit
-                    // );
+                    canvas.drawImage(
+                        piece,
+                        board.unit * x + board.boardX,
+                        board.unit * y + board.boardY,
+                        board.unit,
+                        board.unit
+                    );
                 }
             }
         }
@@ -44,20 +44,20 @@ function draw(board, canvas) {
     for (var x = 0; x < board.gameWidth; x++) {
         color = !color;
         canvas.fillStyle = color ? "#111111" : "#222222";
-        // canvas.fillRect(
-            // board.unit * x + board.boardX,
-            // board.boardHeight + board.boardY + 10,
-            // board.unit,
-            // board.unit
-        // );
+        canvas.fillRect(
+            board.unit * x + board.boardX,
+            board.boardHeight + board.boardY + 10,
+            board.unit,
+            board.unit
+        );
         if (x < board.pieces.length) {
-            // canvas.drawImage(
-                // pieceAsset(board.pieces[x], false),
-                // board.unit * x + board.boardX,
-                // board.boardHeight + board.boardY + 10,
-                // board.unit,
-                // board.unit
-            // );
+            canvas.drawImage(
+                pieceAsset(board.pieces[x], false),
+                board.unit * x + board.boardX,
+                board.boardHeight + board.boardY + 10,
+                board.unit,
+                board.unit
+            );
         }
     }
 }
@@ -66,13 +66,13 @@ function redraw() {
     clear(canvas);
     draw(board, canvas);
     if (mouseDown && ![0, null, undefined].includes(currentPiece)) {
-        // canvas.drawImage(
-            // pieceAsset(currentPiece, false),
-            // mouseX - (board.unit * 0.6),
-            // mouseY - (board.unit * 0.6),
-            // board.unit * 1.2,
-            // board.unit * 1.2
-        // );
+        canvas.drawImage(
+            pieceAsset(currentPiece, false),
+            mouseX - (board.unit * 0.6),
+            mouseY - (board.unit * 0.6),
+            board.unit * 1.2,
+            board.unit * 1.2
+        );
     }
 }
 
