4b4caccab616a3b4a258f1cc0ac4617ab8d8d602
Brandon Roehl <BrandonRoehl@gmail.com>
2016-11-18 16:06:59 -0600

    Default migrations

---
+++ app/views/levels/_level.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! level, :id, :user_id, :inputs, :outputs, :pieces, :width, :height, :name, :description, :created_at, :updated_at
+json.url level_url(level, format: :json)
---
+++ db/migrate/20161118220609_create_levels.rb
@@ -0,0 +1,16 @@
+class CreateLevels < ActiveRecord::Migration[5.0]
+  def change
+    create_table :levels do |t|
+      t.references :user, foreign_key: true
+      t.string :inputs
+      t.string :outputs
+      t.string :pieces
+      t.integer :width
+      t.integer :height
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
