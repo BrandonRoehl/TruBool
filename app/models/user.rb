1b2ae12a121f3e913f0ec62a99b6a44862ad2cf5
Brandon Roehl <BrandonRoehl@gmail.com>
2016-11-23 09:41:18 -0600

    User model complete just finish login now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,26 @@
 class User < ApplicationRecord
     def self.from_oauth(info)
         u = self.find_or_create_by(id: info['id'])
-        # info.permit(
-            # :email,
-            # :given_name,
-            # :family_name,
-            # :link,
-            # :picture
-        # ).each do |k,v|
-            # u.send k, v
-        # end
-        # raise u.inspect
-        # u.save
+        info.slice(
+            'email',
+            'given_name',
+            'family_name',
+            'link',
+            'picture'
+        ).each do |k,v|
+            u.send "#{k}=", v
+        end
+        u.save
         return u
+    end
+
+    def name
+        if self.given_name.blank?
+            return self.family_name
+        elsif self.family_name.blank?
+            return self.given_name
+        else
+            return "#{self.given_name} #{self.family_name}"
+        end
     end
 end
