9c91c08f03274e0519af92070c1b8fbc66e26de6
Brandon Roehl <BrandonRoehl@gmail.com>
2016-11-23 07:18:00 -0600

    Starting the google login stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,36 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    require 'signet/oauth_2/client'
+    protect_from_forgery with: :exception
+    before_action :login, except: [:oauth]
+    def login
+        client_init
+        redirect_to(@client.authorization_uri.to_s)
+    end
 
-  def old
-      render file: "old/index", layout: false
-  end
+    def oauth
+        client_init
+        @client.code = params['code']
+        c = @client.fetch_access_token!
+        raise c.inspect
+    end
+
+    def old
+        render file: "old/index", layout: false
+    end
+
+    private
+
+    def client_init
+        @client = Signet::OAuth2::Client.new(
+            :authorization_uri => 'https://accounts.google.com/o/oauth2/auth',
+            :token_credential_uri =>  'https://www.googleapis.com/oauth2/v3/token',
+            :redirect_uri => 'http://localhost:3000/oauth',
+            :scope => 'email profile',
+
+            :client_id => '831933960744-ltmis3dsek2sn9727ndfmik9fuklg3a4.apps.googleusercontent.com',
+            :project_id => 'trubool-150412',
+            :auth_provider_x509_cert_url => 'https://www.googleapis.com/oauth2/v1/certs',
+            :client_secret => 'sYtRDzWlgUKWNNTlaZjyatsl'
+        )
+    end
 end
