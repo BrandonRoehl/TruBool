1466ab57f4d7af388beb5e6151006d5a750ff9f1
Brandon Roehl <BrandonRoehl@gmail.com>
2016-12-13 17:31:08 -0600

    Working on the add funciton

--- app/assets/javascripts/level_form.js
+++ app/assets/javascripts/level_form.js
@@ -3,10 +3,21 @@ function addInput() {
 
 function clearInput() {
     var node = event.target;
-    while (node.nodeName != "PAPER-INPUT") {
+    while (node.className != "input_set") {
         node = node.parentNode;
         // console.log(node.nodeName);
         // console.log(node);
     }
     node.remove();
+}
+
+function addInput() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("inputs").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "/levels/new/input", true);
+    xhttp.send();
 }
