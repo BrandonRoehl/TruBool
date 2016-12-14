5e6a6c6318b06ac7bbaa5c4f495a48db8b2b29cd
Brandon Roehl <BrandonRoehl@gmail.com>
2016-12-13 22:52:44 -0600

    Done for the four levels

--- app/assets/javascripts/level_form.js
+++ app/assets/javascripts/level_form.js
@@ -15,7 +15,7 @@ function addInput() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("inputs").innerHTML = this.responseText;
+            document.getElementById("inputs").innerHTML += this.responseText;
         }
     };
     xhttp.open("GET", "/levels/new/input", true);
