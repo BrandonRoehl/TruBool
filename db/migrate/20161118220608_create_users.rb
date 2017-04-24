bbeda04d1d4c78ffb0421620a8a2a523ebd7e820
Brandon Roehl <BrandonRoehl@gmail.com>
2017-04-24 18:43:18 -0500

    Migrates for posgress

--- db/migrate/20161118220608_create_users.rb
+++ db/migrate/20161118220608_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
     def change
         create_table :users, id: false do |t|
-            t.integer :id, limit: 16, primary_key: true
+            t.decimal :id, precision: 32, scale: 0, primary_key: true
             t.string :email
             t.string :given_name
             t.string :family_name
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20161118220609) do
     t.index ["user_id"], name: "index_levels_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :decimal, precision: 32, force: :cascade do |t|
     t.string   "email"
     t.string   "given_name"
     t.string   "family_name"
