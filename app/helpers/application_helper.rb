82e11dbd763c6d0ebfe445b622c7fd34a199b420
Brandon Roehl <BrandonRoehl@gmail.com>
2017-04-27 21:05:11 -0500

    Controler testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 module ApplicationHelper
-    def logged_in?
-        return session[:user_id].present?
-    end
+	# $user_id is for testing
+	def logged_in?
+		return session[:user_id].present? || $user_id.present?
+	end
 
-    def current_user
-        if logged_in?
-            return @current_user ||= User.find(session[:user_id])
-        else
-            return nil
-        end
-    end
+	def current_user
+		if logged_in?
+			return @current_user ||= User.find(session[:user_id] || $user_id)
+		else
+			return nil
+		end
+	end
 end
