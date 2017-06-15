3ce9de5c0c2b273f0a1a6154cbc078c848ea6e69
Brandon Roehl <BrandonRoehl@gmail.com>
2017-06-14 23:02:34 -0500

    Starting a generic ajax dialog between polymer and rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,55 @@
 //= require jquery
 //= require jquery_ujs
 // require turbolinks
+
+
+
+window.onload = function(){
+	// Prefer native date pickers but fallback to js ones
+	if(!supportsInputType('date')){
+		$('input[type=date]').fdatepicker({
+			format: 'yyyy-mm-dd'
+		});
+	}
+	if(!supportsInputType('datetime-local')){
+		$('input[type=datetime-local]').fdatepicker({
+			format: 'yyyy-mm-dd hh:ii',
+			pickTime: true
+		});
+	}
+}
+
+function dialogWith(html) {
+	$('#dialogContent').html(html);
+	window.onload();
+}
+function dialogOrEval(xhr) {
+	if(xhr.getResponseHeader('Content-Type').indexOf('text/javascript') == -1) {
+		dialogWith(xhr.responseText);
+	} else {
+		eval(xhr.responseText);
+	}
+}
+// Modal forms for new/edit
+// <%= link_to 'Edit', [:edit, @foo], {remote: true} %>
+$(document).on('ajax:success', 'a[data-remote]', function(e, data, status, xhr) {
+	dialogOrEval(xhr);
+	window.history.pushState({urlPath: e.currentTarget.href}, document.title, e.currentTarget.href);
+});
+$(document).on('ajax:error', 'a[data-remote]', function(e, xhr, status, error) {
+	console.error('Content-Type: '+xhr.getResponseHeader('Content-Type')+'\nStatus: '+status);
+	window.location.href = e.currentTarget.href;
+});
+// Form fields
+// <%= form_for @foo, remote: true do |f| %>
+$(document).on('ajax:success', 'form[data-remote]', function(e, data, status, xhr) {
+	dialogOrEval(xhr);
+});
+$(document).on("ajax:error", 'form[data-remote]', function(e, xhr, status, error) {
+	console.error(error);
+	dialogWith(xhr.responseText);
+});
+$(document).on('ajax:send', 'form[data-remote]', function(xhr) {
+	$(xhr.target).find('[type=submit]').addClass('disabled').val('Saving...');
+});
+
